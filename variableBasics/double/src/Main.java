efb4d1d1dc7932b38fc02abcb3e6c49acfc9111a
Ramona_Ekanayake <ramona.ekanayake@appliedtechnology.se>
2023-01-12 18:25:42 +0100

    Aging as Fine Wine

---
+++ variableBasics/double/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        double birthYear = 1989;
+        double currentYear = 2022;
+        System.out.println("Age as of Today is:" + (currentYear-birthYear) );
+    }
+}
