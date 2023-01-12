91a160de95e20375ab0dc3e2bf1f641cf1ee9c50
Ramona_Ekanayake <ramona.ekanayake@appliedtechnology.se>
2023-01-12 22:21:19 +0100

    Ask for age

--- variableBasics/double/src/Main.java
+++ variableBasics/double/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args)
     {
         double birthYear = 1989;
-        double currentYear = 2022;
+        double currentYear = 2023;
         System.out.println("Age as of Today is:" + (currentYear-birthYear) );
     }
 }
